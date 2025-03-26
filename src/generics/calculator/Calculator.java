b0477ceb862ffefca57b7ec3ac4558ded6204d5f
stekl0 <greystekl0@gmail.com>
2025-03-26 13:11:33 +0500

    refactor divide to improve readability

--- src/generics/calculator/Calculator.java
+++ src/generics/calculator/Calculator.java
@@ -34,30 +34,33 @@ public class Calculator {
   }
 
   public static <T1 extends Number, T2 extends Number> Number divide(T1 a, T2 b) {
-    // Check for division by zero
     if (b.doubleValue() == 0) {
       throw new ArithmeticException("Division by zero is not allowed");
     }
 
     if (a instanceof BigDecimal || b instanceof BigDecimal) {
-      return toBigDecimal(a).divide(toBigDecimal(b), 10, RoundingMode.HALF_UP);
-    } else if (a instanceof Double || b instanceof Double) {
+      return toBigDecimal(a).divide(toBigDecimal(b), 3, RoundingMode.HALF_UP);
+    }
+
+    if (a instanceof Double || b instanceof Double) {
       return a.doubleValue() / b.doubleValue();
-    } else if (a instanceof Float || b instanceof Float) {
+    }
+    if (a instanceof Float || b instanceof Float) {
       return a.floatValue() / b.floatValue();
-    } else if (a instanceof Long || b instanceof Long) {
-      if (a.longValue() % b.longValue() == 0) {
-        return a.longValue() / b.longValue();
-      } else {
-        return (double) a.longValue() / b.longValue();
-      }
+    }
+
+    return integerDivision(a, b);
+  }
+
+  private static <T1 extends Number, T2 extends Number> Number integerDivision(T1 a, T2 b) {
+    if (a instanceof Long || b instanceof Long) {
+      long x = a.longValue();
+      long y = b.longValue();
+      return (x % y == 0) ? (x / y) : ((double) x / y);
     } else {
-      // Return integer if result is whole number, otherwise double
-      if (a.intValue() % b.intValue() == 0) {
-        return a.intValue() / b.intValue();
-      } else {
-        return (double) a.intValue() / b.intValue();
-      }
+      int x = a.intValue();
+      int y = b.intValue();
+      return (x % y == 0) ? (x / y) : ((double) x / y);
     }
   }
 
